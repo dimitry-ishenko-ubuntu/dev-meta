a6802a83ef63fc1f4ff9b707b406328f57cd031f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-08 01:45:10 -0400

    Depend on nvim-lspconfig

---
+++ clangd.lua
@@ -0,0 +1,19 @@
+local prev_on_attach = vim.tbl_get(vim.lsp.config, "clangd", "on_attach")
+
+vim.lsp.config("clangd", {
+    cmd = {"clangd", "--compile-commands-dir=build"},
+    root_markers = {
+        "build",
+        ".clangd",
+        "configure.ac",
+        ".git",
+    },
+    on_attach = function(client, bufnr)
+        if prev_on_attach then
+            prev_on_attach(client, bufnr)
+        end
+        vim.keymap.set("n", "gh", "<cmd>LspClangdSwitchSourceHeader<cr>", {buffer = bufnr})
+    end
+})
+
+vim.lsp.enable("clangd")
