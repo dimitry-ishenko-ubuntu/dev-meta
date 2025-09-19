23b15a70c99a777681ae0c33ec546d5d29d52c8e
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-09-19 09:59:53 -0400

    Not using cmp-nvim anymore

--- clangd.lua
+++ clangd.lua
@@ -16,9 +16,4 @@ vim.lsp.config("clangd", {
     end
 })
 
-local found, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if found then
-    vim.lsp.config("clangd", {capabilities = cmp_nvim_lsp.default_capabilities()})
-end
-
 vim.lsp.enable("clangd")
--- pylsp.lua
+++ pylsp.lua
@@ -1,6 +1,1 @@
-local found, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if found then
-    vim.lsp.config("pylsp", {capabilities = cmp_nvim_lsp.default_capabilities()})
-end
-
 vim.lsp.enable("pylsp")
