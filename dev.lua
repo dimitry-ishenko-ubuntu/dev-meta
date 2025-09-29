bbf636fba7ad8d89fa7fd4befde07105c971847f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-09-29 11:59:35 -0400

    Replaced by devdocs-meta

--- dev.lua
+++ dev.lua
@@ -18,26 +18,6 @@ map("n", "<leader>mb", ":CMake --build build -j `nproc`")
 map("n", "<leader>mg", ":CMake -B build")
 map("n", "<leader>mx", ":CMake -E rm -rf build")
 
--- devdocs
-function CppWord()
-   local isk0 = vim.opt.iskeyword
-   vim.opt.iskeyword:append(":")
-   local cppword = vim.fn.expand("<cword>")
-   vim.opt.iskeyword = isk0
-   return cppword
-end
-
-vim.api.nvim_create_user_command("DevGrep", function(opts)
-   local command = vim.list_extend({"sensible-terminal", "-e", "devgrep"}, opts.fargs)
-   vim.fn.jobstart(command)
-end, {nargs = "*"})
-
-if type(create_alias) == "function" then
-   create_alias("devgrep", "DevGrep")
-end
-
-map("n", "<leader>k", function() vim.cmd("DevGrep " .. CppWord()) end)
-
 -- termdebug
 vim.cmd([[
     packadd termdebug
